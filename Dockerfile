6906add9a81fc0ba6eec0bff5d4e0cca76420fa5
lucky624 <lucky624@github.com>
2021-06-09 14:31:11 +0600

    query save

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.8
 ADD requirements.txt /requirements.txt
-ADD server.py /server.py
-
 RUN pip install -r requirements.txt
+ADD server.py /server.py
 CMD python server.py
--- server.py
+++ server.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'data'
 
 
-@app.route('/', methods=['GET', 'POST', 'PUT'])
+@app.route('/lucky', methods=['GET', 'POST', 'PUT'])
 def listener():
     data = request.get_data()
     files = request.files
-
+    query = request.query_string
     check_file = False
 
     try:
@@ -27,6 +27,12 @@ def listener():
         handle = open(os.path.join(app.config['UPLOAD_FOLDER']) + '/' + "output_" + uuid.uuid4().hex, 'wb')
         handle.write(data)
         handle.close()
+
+    if query:
+        handle = open(os.path.join(app.config['UPLOAD_FOLDER']) + '/' + "output_" + uuid.uuid4().hex, 'wb')
+        handle.write(query)
+        handle.close()
+        pass
 
 
     return "lucky624"
